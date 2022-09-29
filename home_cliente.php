6be076b26f81ea9fe14aeb236e322beb24d31cbd
JanineDiniz <janine.diniz@outlook.com>
2022-09-28 23:28:31 -0300

    atividades

--- home_cliente.php
+++ home_cliente.php
@@ -13,11 +13,50 @@
     <script src="JS/bootstrap.min.js"></script>
 </head>
 <body>
-
     <?php
         include "menu.html";
+    ?>
+    <div class="container mt-5">
+        <h1>Bem-vindo(a) Usuário</h1>
+        <hr class="mt-4">
+        <div class="mt-4" id="atividades">
+            <p id="titulo">Atividade</p>
+            <hr>
+            <div class="ultimas">
+                <div class="ultCerti">
+                    <p class="mb-1">Últimos Certificados Gerados</p>
+                    <div class="arquivos row" id="certificado">
+                        <div class="col-1 content-flex">
+                            <img class="icone" src="assets/icon_certificado1.svg" alt="" srcset="">
+                        </div>
+                        <div class="col-5 content-block d-flex flex-column">
+                            <!-- <img src="assets/InsertImg.svg" alt="" srcset=""> -->
+                            <p class="dados">Nome do Titular</p>    
+                            <p class="dados datas" id="emissao">Data de Emissão: </p> 
+                            <p class="dados datas" id="validade">Data de Validade: </p> 
+                        </div>
+                        <div class="col-3">
+                            <!-- <img src="assets/InsertImg.svg" alt="" srcset=""> -->
+                        </div>
+                        <div class="col-3">
+                            <!-- <img src="assets/InsertImg.svg" alt="" srcset=""> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="ultAssina mt-3">
+                    <p class="mb-1">Últimos Arquivos Assinados</p>
+                    <div class="arquivos" id="assinatura">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
         include "cards.html";
     ?>
+    
     
 </body>
 </html>
