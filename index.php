2389e2fb007570e40800d320b95e8c90c5fd4ef6
Danilo <danilo.justino1@gmail.com>
2022-10-05 21:55:53 -0300

    Troca de logo

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main1.css">
     <link rel="stylesheet" href="css/Inicial.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/logo/2 colors favicon.ico" type="image/x-icon">
+    <link rel="icon" href="assets/logo/2 colors favicon.ico" type="image/x-icon">
+
     <script type="text/javascript" src="js/funcoes.js"></script>
     <title>CyberInterPro - Home</title>
 
